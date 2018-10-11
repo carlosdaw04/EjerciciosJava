3dc998f1bd81365db5becfe436e622c22e6ad9fb
Carlos <costales98@gmail.com>
2018-10-11 14:26:40 +0200

    Correccion

--- Ejercicios_9-10/src/PrecioConIVA.java
+++ Ejercicios_9-10/src/PrecioConIVA.java
@@ -13,20 +13,20 @@ public class PrecioConIVA {
 		precio_sin_iva = teclado.nextFloat();
 		
 		if (precio_sin_iva > 0) {
-			System.out.println("Tipo de IVA: ");
+			System.out.println("Tipo de IVA: (0) 7% - (1) 10% - (2) 21% ");
 			tipo_iva = teclado.nextInt();
 			
-			if(tipo_iva == 4) {
-				pvp = precio_sin_iva + (precio_sin_iva * 0.04f);
-				System.out.printf("El precio de venta final es: " + "%.2f", pvp);
-				
-			}else if(tipo_iva == 7) {
+			if(tipo_iva == 0) {
 				pvp = precio_sin_iva + (precio_sin_iva * 0.07f);
-				System.out.printf("El precio de venta final es: " + "%.2f", pvp);
+				System.out.printf("El precio de venta final es: " + "%.2f", pvp + ":");
+				
+			}else if(tipo_iva == 1) {
+				pvp = precio_sin_iva + (precio_sin_iva * 0.1f);
+				System.out.printf("El precio de venta final es: " + "%.2f", pvp + ":");
 								
-			}else if(tipo_iva == 16) {
-				pvp = precio_sin_iva + (precio_sin_iva * 0.16f);
-				System.out.printf("El precio de venta final es: " + "%.2f", pvp );
+			}else if(tipo_iva == 2) {
+				pvp = precio_sin_iva + (precio_sin_iva * 0.21f);
+				System.out.printf("El precio de venta final es: " + "%.2f", pvp + ":");
 			}else {
 				System.out.println("Tipo de IVA incorrecto.");
 			}
