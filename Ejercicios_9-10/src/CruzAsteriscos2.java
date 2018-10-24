3bb9940b659eb1a298f31f918de0c3a7d21beaa9
Carlos <costales98@gmail.com>
2018-10-24 14:17:36 +0200

    Actualizacion.

--- Ejercicios_9-10/src/CruzAsteriscos2.java
+++ Ejercicios_9-10/src/CruzAsteriscos2.java
@@ -19,5 +19,23 @@ public class CruzAsteriscos2 {
 	public static void main(String[] args) {
 		int n = obtenerFC("filas");
 		int m = obtenerFC("columnas");
+		
+		for (int i = 0; i<n; i++) {
+			if(i == n/2) {
+				for(int j = 0; j<m; j++) {
+					System.out.print("*");
+				}
+			}
+			else {
+				for(int j = 0; j<n; j++) {
+					if(j == m/2) {
+						System.out.print("*");
+					}else {
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println();
+		}
 	}
 }
