cb88678ae44934611b5e0414f61f5aeb4d569894
Carlos <costales98@gmail.com>
2018-11-22 23:21:25 +0100

    Commit

--- EjerciciosArrays/src/Prueba.java
+++ EjerciciosArrays/src/Prueba.java
@@ -2,22 +2,27 @@
 public class Prueba {
 	public static void main(String[] args) {
 		int[] x = { 47, 31, 2, 5 };
+		int[] y = new int[4];
 		// System.out.println(x[0] * 4);
 		for (int i = 0; i < x.length; i++) {
-				System.out.println(x[i] * 4);	
+				System.out.print(x[i] * 4 + " ");	
 			
 		}
 
 		System.out.println();
 
 		for (int i = x.length - 1; i >= 0; i--) {
-			System.out.println(x[i] * 4);
+			y[i] = x[i] * 4;
+		}
+		
+		for (int i = x.length - 1; i >= 0; i--) {
+			System.out.print(y[i] + " ");
 		}
 		
 		System.out.println();
 		
-		for (int e: x) {	//bucle abreviado para array
-			System.out.println(e * 4);
-		}
+//		for (int e: x) {	//bucle abreviado para array
+//			System.out.println(e * 4);
+//		}
 	}
 }
